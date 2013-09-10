1032a4198fda8d764356eee3d175b31954e72379
panchenji <panchenji@gmail.com>
2013-09-10 00:56:10 -0500

    Fix bug

--- src/DistributedLogTest.java
+++ src/DistributedLogTest.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -16,28 +17,26 @@ import static org.junit.Assert.assertEquals;
 */
 public class DistributedLogTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    DistributedLog log;
+    Thread t;
     @Before
     public void setUp() throws Exception {
         System.setOut(new PrintStream(outContent));
+        this.log = new DistributedLog(0);
+        this.t = new Thread(log);
+        this.t.start();
+        sleep(1000);
+        ByteArrayInputStream in1 = new ByteArrayInputStream(" ".getBytes());
+        System.setIn(in1);
     }
 
     @org.junit.Test
     public void testInfrequent() throws Exception {
-      DistributedLog log = new DistributedLog(0);
-        Thread t = new Thread(log);
-        t.start();
+
         ByteArrayInputStream in = new ByteArrayInputStream("key=key1;machine=0;".getBytes());
         System.setIn(in);
-        boolean cap = outContent.toString().indexOf("key1:value1") > 0;
+        boolean cap = outContent.toString().indexOf("key1:value1") >= 0;
         assertEquals(true, cap);
-    }
-    @org.junit.Test(expected = Exception.class)
-    public void testInfrequentException() throws Exception {
-        DistributedLog log = new DistributedLog(0);
-        Thread t = new Thread(log);
-        t.start();
-        ByteArrayInputStream in = new ByteArrayInputStream("key=key1;machine=1;".getBytes());
-        System.setIn(in);
     }
 
     @Test
